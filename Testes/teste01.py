385188a9079cc45dafb83e0039d7140963cfad5f
arijunior09 <arioliveirajuniorjose@gmail.com>
2026-02-14 21:30:34 -0300

    update

--- Testes/teste01.py
+++ Testes/teste01.py
@@ -4,12 +4,17 @@ def main(page: ft.Page):
     page.title = "Teste 01"
     page.window.width = 400
     page.window.height = 300
-    page.padding = ft.padding.all(20)
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    def on_click(e):
-        print("Botão clicado!")
+    linha01 = ft.Row(controls=[ft.Text("Lin1 Col01| "), ft.Text("Lin1 Col02| "), ft.Text("Lin1 Col03| ")])
+    linha02 = ft.Row(controls=[ft.Text("Lin2 Col01| "), ft.Text("Lin2 Col02| "), ft.Text("Lin2 Col03| ")])
+    linha03 = ft.Row(controls=[ft.Text("Lin3 Col01| "), ft.Text("Lin3 Col02| "), ft.Text("Lin3 Col03| ")])
 
-    button = ft.ElevatedButton(text="Clique aqui", on_click=on_click)
-    page.add(button)
+    coluna = ft.Column(
+        controls=[linha01,
+                  linha02,
+                  linha03]
+    )
+
+    page.add(coluna)
+
+ft.app(target=main)
